ec2a20f4317e1a2081b177aae3b9a4b2934a6a46
Shirshendu Saha <shirshendusaha78@gmail.com>
2022-08-26 19:28:43 +0530

    Commit @ 2022-08-26 19:28:43

--- 1_3B.py
+++ 1_3B.py
@@ -1,10 +1,10 @@
 import numpy as np
 import matplotlib.pyplot as plt
 
-H = 70.0 * 1e3 / (3.08e16)
+H = 70 / 3.08e13
 wm = 1.0
-ai = 1e-10 # 0.00
-af = 1.00
+a0 = 1e-10 # 0.00
+a1 = 1.00
 n = 100
 t0 = 0
 
@@ -26,7 +26,7 @@ def rk4(f, a, b, n, ic):
         y[i + 1] = y[i] + h * f(x[i] + 0.5 * h, y[i] + 0.5 * h * f(x[i], y[i]))
     return [x, y]
 
-[a, t] = rk4(f, ai, af, n, t0)
+[a, t] = rk4(f, a0, a1, n, t0)
 
 tr = g(a)
 
