c878a6721a341bc4a7899c239dbfc3b2c0a4d625
Shirshendu Saha <shirshendusaha78@gmail.com>
2022-08-29 18:12:34 +0530

    Commit @ 2022-08-29 18:12:34

--- 1_3B.py
+++ 1_3B.py
@@ -7,13 +7,11 @@ a0 = 0.0
 a1 = 1.0
 n = 100
 
-# x -> a, y -> t
-def f(x, y):
-    return x**0.5 / (H * wm**0.5)
+def f(a, t):
+    return a**0.5 / (H * wm**0.5)
 
-
-def g(x):
-    return 2 * x**1.5 / (3 * H * wm**0.5)
+def g(a):
+    return 2 * a**1.5 / (3 * H * wm**0.5)
 
 h = (a1 - a0) / (n - 1)
 a = np.linspace(a0, a1, n)
--- 1_3D.py
+++ 1_3D.py
@@ -2,16 +2,15 @@ import numpy as np
 import matplotlib.pyplot as plt
 
 H = 70 / 3.086e19
-wm = 0.9
+wm = 0.3
 wr = 5e-5
-a0 = 0.0
+k = -0.3
+a0 = 1e-10
 a1 = 1.0
 n = 100
-k = -0.9
 
-# x -> a, y -> t
-def f(x, y):
-    return 1 / (H ** 2 * (wm / x + wr / x ** 2) - k)
+def f(a, t):
+    return 1 / (H ** 2 * (wm / a + wr / a ** 2 - k))
 
 h = (a1 - a0) / (n - 1)
 a = np.linspace(a0, a1, n)
