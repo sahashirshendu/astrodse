0de4c9d13f0416d18a27cb0581ccb102ce6bcc2e
Shirshendu Saha <shirshendusaha78@gmail.com>
2022-09-17 13:07:26 +0530

    .

---
+++ test.sh
@@ -0,0 +1,4 @@
+git config --global user.email "sayan.jarvis@gmail.com"
+git config --global user.name "maitysayankumar"
+git config --global credential.helper store
+git add . && git commit -m "." && git push
