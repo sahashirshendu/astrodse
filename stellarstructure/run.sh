de1f79e40f699f6a00ddfc2091c665d0707bba64
Shirshendu Saha <shirshendusaha78@gmail.com>
2022-10-22 21:40:30 +0530

    Commit @ 2022-10-22 21:40:30

--- stellarstructure/run.sh
+++ stellarstructure/run.sh
@@ -1,5 +1,5 @@
 mkdir -p data
-g++ -c SSFunctions.cpp
-g++ -c main.cpp
-g++ -c Initialize.cpp
-g++ main.o SSFunctions.o Initialize.o
+c++ -c SSFunctions.cpp
+c++ -c main.cpp
+c++ -c Initialize.cpp
+c++ main.o SSFunctions.o Initialize.o
