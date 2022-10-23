86efe756157cab4e3d59b8ba8d9c4ea8daaba33d
Shirshendu Saha <shirshendusaha78@gmail.com>
2022-10-23 12:20:45 +0530

    Commit @ 2022-10-23 12:20:45

--- lab/5ef.py
+++ lab/5ef.py
@@ -6,12 +6,12 @@ v = data[:, 1]
 d = 116
 V = v + 5 - 5 * log10(d)
 
-grid()
 xlabel('B-V')
 ylabel('$M_v$')
 ylim(8,-4)
 plot(b-v,V,'b.')
-scatter(0.7,4.74,marker='*')
+scatter(0.7,4.74,marker='*',label='Sun')
 xticks([-.35,-.31,-.16,0,.13,.27,.42,.58,.7,.89,1.18,1.45,1.63],['O5','B0','B5','A0','A5','F0','F5','G0','G5','K0','K5','M0','M5'])
 title('Pleiades Cluster')
+legend()
 show()
