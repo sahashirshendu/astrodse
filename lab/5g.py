b7bb871aa97cb1805b56780c399caef48a5a1ef2
Shirshendu Saha <shirshendusaha78@gmail.com>
2022-10-23 12:05:08 +0530

    Commit @ 2022-10-23 12:05:08

--- lab/5g.py
+++ lab/5g.py
@@ -1,15 +1,15 @@
 from pylab import *
 
-data = loadtxt('pleiades.txt')
+data = loadtxt('ngc104.txt')
 b = data[:, 0]
 v = data[:, 1]
-d = 116
+d = 4450
 V = v + 5 - 5 * log10(d)
 
 grid()
 xlabel('B-V')
 ylabel('$M_v$')
-ylim(8,-4)
+ylim(12,-2)
 plot(b-v,V,'b.')
-title('Pleiades Cluster')
+title('47 Tuc Cluster')
 show()
