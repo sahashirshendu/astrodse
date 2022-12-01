6428aacd1ef00a2426100c1fa3d059e87675deae
Shirshendu Saha <shirshendusaha78@gmail.com>
2022-12-01 22:05:25 +0530

    Commit @ 2022-12-01 22:05:25

--- 2a.py
+++ 2a.py
@@ -2,15 +2,14 @@ from pylab import *
 
 H_0 = 70/3.086e19
 c = 3e8
-dh = (c/H_0) / 3.086e22
+dh = (c/H_0)/3.086e22
 th = 1/H_0
-
 wm = 0.0
 wr = 1./3.
 z = 5.0
 n = 1000
 
-zp = linspace(0, z, n)
+zp = linspace(0,z,n)
 dpm = zeros(n)
 dam = zeros(n)
 dlm = zeros(n)
@@ -19,18 +18,18 @@ dar = zeros(n)
 dlr = zeros(n)
 
 def dpp(z, w):
-    return 2 * dh / (3 * w + 1) * (1 - (1 + z) ** (- (3 * w + 1) / 2))
+    return 2*dh/(3*w+1)*(1-(1+z)**(-(3*w+1)/2))
 
 for i in range(n):
-    dpm[i] = dpp(zp[i], wm)
-    dpr[i] = dpp(zp[i], wr)
-    dam[i] = dpm[i] / (1 + zp[i])
-    dar[i] = dpr[i] / (1 + zp[i])
-    dlm[i] = dpm[i] * (1 + zp[i])
-    dlr[i] = dpr[i] * (1 + zp[i])
+    dpm[i] = dpp(zp[i],wm)
+    dpr[i] = dpp(zp[i],wr)
+    dam[i] = dpm[i]/(1+zp[i])
+    dar[i] = dpr[i]/(1+zp[i])
+    dlm[i] = dpm[i]*(1+zp[i])
+    dlr[i] = dpr[i]*(1+zp[i])
 
-agem = (th * 2 / (3*(1+wm))) / (pi*1e16)
-ager = (th * 2 / (3*(1+wr))) / (pi*1e16)
+agem=(th*2/(3*(1+wm)))/(pi*1e16)
+ager=(th*2/(3*(1+wr)))/(pi*1e16)
 
 print("Age of matter only universe =",agem,"billion years")
 print("Age of radiation only universe =",ager,"billion years")
